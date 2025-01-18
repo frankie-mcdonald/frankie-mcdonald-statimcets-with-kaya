08304aafb46ec2bc5006215dd5775787e847e73e
Frankie <frankie.m.18@hotmail.com>
2025-01-18 15:59:06 -0800

    alphabet page layout almost done, needs responsiveness added

--- src/pages/AlphabetPage/AlphabetPage.jsx
+++ src/pages/AlphabetPage/AlphabetPage.jsx
@@ -3,18 +3,20 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import NavBar from "../../components/NavBar/NavBar";
+import AlphabetCard from "../../components/AlphabetCard/AlphabetCard";
 
 function AlphabetPage() {
   const baseURL = import.meta.env.VITE_API_URL;
   const [alphabet, setAlphabet] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedLetterData, setSelectedLetterData] = useState(null); // State for selected letter object
 
   useEffect(() => {
     async function getAlphabet() {
       try {
         const response = await axios.get(`${baseURL}/alphabet`);
         setAlphabet(response.data);
-        console.log(response.data);
+        // console.log(response.data);
       } catch (error) {
         console.log("Error fetching alphabet", error);
       } finally {
@@ -23,21 +25,46 @@ function AlphabetPage() {
     }
     getAlphabet();
   }, []);
+
   if (isLoading) {
     return <p>Loading alphabet</p>;
   }
+
+  const handleLetterClick = (letterData) => {
+    setSelectedLetterData(letterData);
+  };
+  const playAudio = (audioUrl) => {
+    const fullUrl = audioUrl.startsWith("http")
+      ? audioUrl
+      : `${baseURL}${audioUrl}`;
+    const audio = new Audio(fullUrl);
+
+    audio.play().catch((error) => {
+      console.error("Error playing audio:", error);
+    });
+  };
   return (
     <>
       <Header />
+      <h1 className="alphabet__header">St̓át̓imcets Alphabet</h1>
       <section className="alphabet">
+        {selectedLetterData && <AlphabetCard details={selectedLetterData} />}
+        <h2 className="alphabet__subheader">
+          Click a letter to hear the pronunciation
+        </h2>
         <ul className="alphabet__list">
-          {alphabet.map((alphabet) => (
-            <li key={alphabet.id} className="alphabet__item">
-              {alphabet.letter}
+          {alphabet.map((item) => (
+            <li
+              key={item.id}
+              className="alphabet__item"
+              onClick={() => handleLetterClick(item)}
+            >
+              {item.letter}
             </li>
           ))}
         </ul>
       </section>
+
       <NavBar />
     </>
   );
