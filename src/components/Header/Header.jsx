036d1175bcbfc62451b9a3d3265c9ce611914c51
Frankie <frankie.m.18@hotmail.com>
2025-01-19 22:17:44 -0800

    code clean up. px to rem. small color adjustments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import BackButton from "../BackButton/BackButton";
 import "./Header.scss";
-
+import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <nav className="header">
       <BackButton />
-      <h1 className="header__title">St̓át̓imcets with Kaya</h1>
+      <NavLink to="/">
+        <h1 className="header__title">St̓át̓imcets with Kaya</h1>
+      </NavLink>
     </nav>
   );
 }
