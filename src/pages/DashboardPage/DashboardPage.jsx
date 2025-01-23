2447dd2a3e789fcc1f2fbed5c2f0f37117340dba
Frankie <frankie.m.18@hotmail.com>
2025-01-22 21:09:49 -0800

    added login and dashboard, more button animation, adjust responsiveness

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,20 @@
+import DashboardCard from "../../components/DashboardCard/DashboardCard";
+import Header from "../../components/Header/Header";
+import LoginCard from "../../components/LoginCard/LoginCard";
+import NavBar from "../../components/NavBar/NavBar";
 import "./DashboardPage.scss";
 
 function DashboardPage() {
-  return <div>Login</div>;
+  return (
+    <>
+      <Header />
+      <section className="dashboard">
+        {/* <LoginCard /> */}
+        <DashboardCard />
+      </section>
+      <NavBar />
+    </>
+  );
 }
 
 export default DashboardPage;
